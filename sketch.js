3f73fb6103861220e2d58d35136bb7d5027dad56
ff6347 <hash@ff6347.email>
2023-07-23 03:42:47 +0000

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+/// <reference path="./node_modules/@types/p5/lib/addons/p5.sound.d.ts" />
+/// <reference path="./node_modules/@types/p5/global.d.ts" />
+/// <reference path="./node_modules/@types/p5/literals.d.ts" />
+/// <reference path="./node_modules/@types/p5/constants.d.ts" />
+// Keep these comments alive.
+// They will help you while writing code.
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(128);
+}
+
+function draw() {}
